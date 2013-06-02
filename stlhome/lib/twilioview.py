9443bb75e9151c6307a5353b44d77e1d5f59388e
Brian Hicks <brian.hicks@rockfishinteractive.com>
2013-06-02 03:45:43 -0500

    add twilio view

---
+++ stlhome/lib/twilioview.py
@@ -0,0 +1,12 @@
+from django.views.generic import View
+from django.http import HttpResponse
+
+class TwilioView(View):
+    '''return a twillio XML response'''
+    def dispatch(self, *args, **kwargs):
+        'dispatch based on verb'
+        twilio_resp = super(TwilioResponse, self).dispatch(*args, **kwargs)
+        return HttpResponse(
+            unicode(twilio_resp),
+            mimetype='application/xml',
+        )
