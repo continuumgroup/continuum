ab29fa044f474b284fd3a0518f4d9864c975109d
Brian Hicks <brian.hicks@rockfishinteractive.com>
2013-06-02 06:56:30 -0500

    fix url

--- stlhome/apps/phone/urls.py
+++ stlhome/apps/phone/urls.py
@@ -8,5 +8,6 @@ urlpatterns = patterns(
     url(r'^operator/$', OperatorView.as_view(), name='operator'),
     url(r'^collect_location/$', CollectLocationView.as_view(), name='collect_location'),
     url(r'^bed_count/$', BedCountView.as_view(), name='bed_count'),
+    url(r'^find_shelter/$', FindShelterView.as_view(), name='find_shelter'),
     url(r'^start_shelter_call/$', ShelterCallView.as_view(), name='start_shelter_call'),
 )
