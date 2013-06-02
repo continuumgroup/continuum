2fb15a1247ef76a417629ba19a720c7303445802
Brian Hicks <brian.hicks@rockfishinteractive.com>
2013-06-02 08:59:14 -0500

    change url

--- stlhome/apps/phone/urls.py
+++ stlhome/apps/phone/urls.py
@@ -11,5 +11,5 @@ urlpatterns = patterns(
     url(r'^find_shelter/$', FindShelterView.as_view(), name='find_shelter'),
     url(r'^start_shelter_call/(?P<client_call>\d+)/(?P<pks>.+)/$', ShelterCallView.as_view(), name='start_shelter_call'),
     url(r'^post_shelter_call/(?P<client_call>\d+)/(?P<pks>.*)/$', PostShelterCallView.as_view(), name='post_shelter_call'),
-    url(r'^verify_shelter_availability/(?P<client_call>\d+)/$', VerifyShelterAvailabilityView.as_view(), name='start_shelter_call'),
+    url(r'^verify_shelter_availability/(?P<client_call>\d+)/$', VerifyShelterAvailabilityView.as_view(), name='verify_shelter_availability'),
 )
