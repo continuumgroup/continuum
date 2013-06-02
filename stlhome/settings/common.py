a37bf0d962c34253180ac21344f9859eff8af793
Brian Hicks <brian.hicks@rockfishinteractive.com>
2013-06-01 22:35:53 -0500

    remove deprecation warning by adding false filter

--- stlhome/settings/common.py
+++ stlhome/settings/common.py
@@ -218,6 +218,7 @@ LOGGING = {
     'handlers': {
         'mail_admins': {
             'level': 'ERROR',
+            'filters': [],
             'class': 'django.utils.log.AdminEmailHandler'
         }
     },
