ca07bafb0f35793f144a3a7a305bcb0441dbb8d9
Brian Hicks <brian.hicks@rockfishinteractive.com>
2013-06-04 20:28:03 -0500

    add created_at, updated_at, and call_state

--- stlhome/apps/phone/models.py
+++ stlhome/apps/phone/models.py
@@ -1,12 +1,18 @@
 from django.db import models
+from django_fsm.db.fields import FSMField, transition
 
 from stlhome.apps.shelters.models import Shelter
 
 class Call(models.Model):
     'call from Twilio'
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
     sid = models.CharField(max_length=34)
     bed_count = models.PositiveIntegerField(default=0)
     shelter = models.ForeignKey(Shelter, null=True)
 
     location_name = models.CharField(max_length=300, null=True, blank=True)
     client_name = models.CharField(max_length=300, null=True, blank=True)
+
+    call_state = FSMField(default='welcome')
