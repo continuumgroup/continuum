b7250fa4bc131064663dd7d08564f66a288db1b1
Brian Hicks <brian.hicks@rockfishinteractive.com>
2013-06-05 21:48:57 -0500

    use environment variable to detect CI environment

--- stlhome/lib/twilioclient.py
+++ stlhome/lib/twilioclient.py
@@ -1,4 +1,9 @@
+from os import environ
 from django.conf import settings
 from twilio.rest import TwilioRestClient
 
-client = TwilioRestClient(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
+if 'CI' in environ:  # mock things out to test anyway, please
+    from mock import Mock
+    client = Mock()
+else:
+    client = TwilioRestClient(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
