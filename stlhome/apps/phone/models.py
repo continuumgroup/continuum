bccd63849a87444967df099209e1dbd0bbd997d7
Brian Hicks <brian.hicks@rockfishinteractive.com>
2013-06-05 19:29:33 -0500

    finish testing ClientCall

--- stlhome/apps/phone/models.py
+++ stlhome/apps/phone/models.py
@@ -1,10 +1,13 @@
+from django.contrib.sites.models import Site
 from django.core.urlresolvers import reverse
 from django.db import models
 from django.utils.translation import ugettext as _
 from django_fsm.db.fields import FSMField, transition
 from twilio.twiml import Response
+from urlparse import urljoin
 
 from stlhome.apps.shelters.models import Shelter
+from stlhome.lib.twilioclient import client
 
 class ClientCall(models.Model):
     'call from Twilio'
@@ -20,15 +23,64 @@ class ClientCall(models.Model):
 
     call_state = FSMField(default='welcome')
 
+    # states from which more data can be collected
+    COLLECT_DATA_STATES = [
+        'welcome',
+        'processed_location',
+        'processed_name',
+        'processed_bed_count',
+    ]
+
     @transition(source='*', target='operator', save=True)
     def transfer_to_operator(self):
         '''transfer/connect to an operator, this class does not concern itself with further steps'''
 
-    @transition(source='welcome', target='requested_location', save=True)
+    @transition(source='*', target='enqueued', save=True)
+    def enqueue(self):
+        '''"enqueue" this call (put on hold)'''
+
+    @transition(source='enqueued', target='dequeued', save=True)
+    def dequeue(self, url, method='GET'):
+        '''"dequeue" this call (take off hold)'''
+        site = Site.objects.get_current()
+        client.calls.route(
+            sid=self.sid,
+            method=method,
+            url=urljoin('http://' + site.domain, url)
+        )
+
+    @transition(source='dequeued', target='confirmed', save=True)
+    def confirm(self):
+        '''confirm shelter placement'''
+
+    @transition(source='*', target='declined', save=True)
+    def decline(self):
+        '''decline shelter placement (no match found/rejection)'''
+
+    @transition(source=COLLECT_DATA_STATES, target='requested_location', save=True)
     def request_location(self):
         '''request client location, via voice'''
 
     @transition(source='requested_location', target='processed_location', save=True)
     def process_location(self, request):
         '''process location from a given request'''
+        # TODO: fill stub with appropriate GIS conversion
         self.location_name = request.POST.get('RecordingUrl', '')
+
+    @transition(source=COLLECT_DATA_STATES, target='requested_name', save=True)
+    def request_name(self):
+        '''request client name, via voice'''
+
+    @transition(source='requested_name', target='processed_name', save=True)
+    def process_name(self, request):
+        '''process name from a given request'''
+        self.client_name = request.POST.get('RecordingUrl', '')
+
+    @transition(source=COLLECT_DATA_STATES, target='requested_bed_count', save=True)
+    def request_bed_count(self):
+        '''request number of beds required'''
+
+    @transition(source='requested_bed_count', target='processed_bed_count', save=True)
+    def process_bed_count(self, request):
+        '''process beds from a given request'''
+        self.bed_count = int(request.POST['Digits'])
--- stlhome/apps/phone/tests/models_tests.py
+++ stlhome/apps/phone/tests/models_tests.py
@@ -1,3 +1,5 @@
+from mock import patch
+
 from stlhome.lib.test import BaseTest
 from ..models import ClientCall
 
@@ -7,7 +9,7 @@ class ClientCallTests(BaseTest):
     def verb_classes(self, resp):
         return [type(verb) for verb in resp.verbs]
 
-    def assertFromTo(self, source, method_name, target, *args, **kwargs):
+    def assertTransition(self, source, method_name, target, *args, **kwargs):
         cc = self.deliver(ClientCall, call_state=source)
         getattr(cc, method_name)(*args, **kwargs)
 
@@ -16,21 +18,74 @@ class ClientCallTests(BaseTest):
         return cc
 
     def test_transfer_to_operator_from_welcome(self):
-        'transfer_to_operator available from welcome'
-        self.assertFromTo('welcome', 'transfer_to_operator', 'operator')
+        '''transfer_to_operator available from welcome'''
+        self.assertTransition('welcome', 'transfer_to_operator', 'operator')
 
     def test_transfer_to_operator_from_other(self):
-        'transfer_to_operator available from anywhere'
-        self.assertFromTo('does_not_exist_1234', 'transfer_to_operator', 'operator')
+        '''transfer_to_operator available from anywhere'''
+        self.assertTransition('does_not_exist_1234', 'transfer_to_operator', 'operator')
 
     def test_request_location(self):
-        'request_location available from welcome'
-        self.assertFromTo('welcome', 'request_location', 'requested_location')
+        '''request_location available from COLLECT_DATA_STATES'''
+        for state in ClientCall.COLLECT_DATA_STATES:
+            self.assertTransition(state, 'request_location', 'requested_location')
 
     def test_process_location(self):
-        '''process_location pulls relevant data out of view and uses it'''
-        cc = self.assertFromTo(
+        '''process_location pulls relevant data out of a request and uses it'''
+        cc = self.assertTransition(
             'requested_location', 'process_location', 'processed_location', 
             request=self.factory.post('/', {'RecordingUrl': 'test'}),
         )
         self.assertEqual('test', cc.location_name)
+
+    def test_request_name(self):
+        '''request_name available from COLLECT_DATA_STATES'''
+        for state in ClientCall.COLLECT_DATA_STATES:
+            self.assertTransition(state, 'request_name', 'requested_name')
+
+    def test_process_name(self):
+        '''process_name pulls relevant data out of a request and uses it'''
+        cc = self.assertTransition(
+            'requested_name', 'process_name', 'processed_name',
+            request=self.factory.post('/', {'RecordingUrl': 'test'}),
+        )
+        self.assertEqual('test', cc.client_name)
+
+    def test_request_bed_count(self):
+        '''request_bed_count available from COLLECT_DATA_STATES'''
+        for state in ClientCall.COLLECT_DATA_STATES:
+            self.assertTransition(state, 'request_bed_count', 'requested_bed_count')
+
+    def test_process_bed_count(self):
+        '''request_bed_count pulls relevant data out of a request and uses it'''
+        cc = self.assertTransition(
+            'requested_bed_count', 'process_bed_count', 'processed_bed_count',
+            request=self.factory.post('/', {'Digits': '2'})
+        )
+
+        self.assertEqual(2, cc.bed_count)
+
+    def test_enqueue(self):
+        '''enqueue from anywhere'''
+        self.assertTransition('welcome', 'enqueue', 'enqueued')
+
+    def test_confirm(self):
+        '''confirm from dequeued'''
+        self.assertTransition('dequeued', 'confirm', 'confirmed')
+
+    def test_decline(self):
+        '''decline from anywhere'''
+        self.assertTransition('welcome', 'decline', 'declined')
+
+    @patch('stlhome.apps.phone.models.client')
+    def test_dequeued(self, mock_client):
+        cc = self.assertTransition(
+            'enqueued', 'dequeue', 'dequeued',
+            url='test', method='TEST'
+        )
+
+        mock_client.calls.route.assert_called_with(
+            sid=cc.sid,
+            method='TEST',
+            url='http://example.com/test',
+        )
