5ac336d1790bae16fee1369c29bb6e879eaebd05
Brian Hicks <brian.hicks@rockfishinteractive.com>
2013-07-08 06:01:58 -0500

    add test for good behavior

--- continuum/libs/tests/geocoding_tests.py
+++ continuum/libs/tests/geocoding_tests.py
@@ -30,3 +30,17 @@ class AddressToLatlngTests(BaseTest):
         )
 
         self.assertRaises(BadDataError, address_to_latlng, '')
+
+    @httpretty.activate
+    def test_returns_good_data(self):
+        'returns latlng pair for good data'
+        httpretty.register_uri(
+            httpretty.GET, settings.GEOCODING_API_ENDPOINT,
+            body='{"results" : [{"geometry" : {"location" : {"lat" : 38.7244210, "lng" : -90.34697299999999}}}], "status" : "OK"}',
+            content_type='application/json'
+        )
+
+        self.assertEqual(
+            (38.7244210, -90.34697299999999),
+            address_to_latlng('123 Main Street, 63134') # will return correctly here because of patch
+        )
