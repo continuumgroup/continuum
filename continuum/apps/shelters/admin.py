327eba05776a1da26b3f0623172152bfd0d9034f
Brian Hicks <brian.hicks@rockfishinteractive.com>
2013-07-08 06:47:30 -0500

    add admin for shelters

---
+++ continuum/apps/shelters/admin.py
@@ -0,0 +1,24 @@
+'admin for shelter models'
+from django.contrib import admin
+
+from .models import Shelter, Availability
+
+class ShelterAdmin(admin.ModelAdmin):
+    fieldsets = (
+        ('Basic Information', {
+            'fields': ('name', 'address', 'phone_number')
+        }),
+        ('Filtering', {
+            'fields': ('classifier_action',),
+            'classes': ['collapse'],
+        }),
+        ('Location', {
+            'fields': (('latitude', 'longitude'),),
+            'classes': ['collapse'],
+        })
+    )
+
+    list_display = ('name', 'address', 'phone_number')
+    search_fields = ('name', 'address', 'phone_number')
+
+admin.site.register(Shelter, ShelterAdmin)
